fad7ad6c66bf8712c90f6b70ab130a928b0e844b
Tony van Riet <tony.van.riet@nfiindustries.com>
2020-02-07 10:28:37 -0600

    Fix test input

--- example/test/example_test.exs
+++ example/test/example_test.exs
@@ -3,15 +3,13 @@ defmodule ExampleTest do
   doctest Example
 
   test "greets the world" do
-    trigger_json = """
-    {
-      "body": {
-          "command_type": "Hackathon.Counter.IncrementCount",
-          "version": 1,
-          "counter_id": "asdf"
-        }
+    trigger_json = %{
+      "body" => %{
+        "command_type" => "Hackathon.Counter.IncrementCount",
+        "version" => 1,
+        "counter_id" => "asdf"
+      }
     }
-    """
 
     assert Example.hello(trigger_json, %{}) == {:ok, %{}}
   end
