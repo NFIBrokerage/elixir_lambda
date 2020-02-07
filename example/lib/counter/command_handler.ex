1ac44f9bc7c16cc94d795f45eef248c1b51d6e44
Tony van Riet <tony.van.riet@nfiindustries.com>
2020-02-07 14:06:09 -0600

    Log event history fetch

--- example/lib/counter/command_handler.ex
+++ example/lib/counter/command_handler.ex
@@ -9,6 +9,10 @@ defmodule Hackathon.Counter.CommandHandler do
     Logger.debug(fn -> "#{inspect(__MODULE__)} handling #{inspect(command)}" end)
 
     instance_id = command.counter_id
+
+    event_history = AggregateRepo.fetch_aggregate_event_history(instance_id)
+    Logger.info(fn -> "fetched event history for #{instance_id}: inspect(event_history)" end)
+
     aggregate_state = AggregateRepo.fetch_aggregate_state(instance_id)
     {:ok, events} = Decider.decide(aggregate_state, command)
     :ok = AggregateRepo.persist_events(events, instance_id)
